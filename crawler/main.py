56743334f30ad94d789de2b25ad42301cea01694
Sapfir0 <sapfir999999@yandex.ru>
2020-05-07 02:25:26 +0400

    Добавил поддержку монги

--- crawler/main.py
+++ crawler/main.py
@@ -1,5 +1,5 @@
 import asyncio
-# from aiomisc import entrypoint
+from model import Connection, insertMongo
 from crawler import download_news
 from files import writeJSON
 import sys
@@ -7,17 +7,20 @@ sys.path.append("..")
 from config import DATA_PATH
 
 
-async def queue_printer(queue):
+async def queue_printer(conn, queue):
     while True:
         val = await queue.get()
         writeJSON(DATA_PATH, val)
+        id = insertMongo(conn, "mydatabase", "customers", val)
         queue.task_done()
 
 
 async def main():
+    conn = Connection()
+
     queue = asyncio.Queue()
     downloader = asyncio.create_task(download_news(queue))
-    printer = asyncio.create_task(queue_printer(queue))
+    printer = asyncio.create_task(queue_printer(conn, queue))
     await asyncio.gather(downloader)
     await queue.join()
     printer.cancel()
--- crawler/model.py
+++ crawler/model.py
@@ -1,6 +1,26 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import MongoClient
 
-# TODO: make async model
+# from motor.motor_asyncio import AsyncIOMotorClient
+# # TODO: make async model | Нет.
+# #uri = "mongodb://dev:dev@localhost:27017/mydatabase?authSource=admin"
+# client = AsyncIOMotorClient(uri)
 
-uri = "mongodb://dev:dev@localhost:27017/mydatabase?authSource=admin"
-client = AsyncIOMotorClient(uri)
+
+class Connection:
+    uri = ""
+    conn = ""
+
+    def __init__(self, host="localhost", port="27017"):
+        self.uri = f"mongodb://{host}:{port}"
+        self.conn = MongoClient(self.uri)  # можно и не передавать, тут по умолчанию эти данные
+
+    def getConnection(self):
+        return self.conn
+
+
+def insertMongo(connection, databaseName: str, tableName: str, data):
+    mydb = connection.conn[databaseName]
+    mycol = mydb[tableName]
+    insertedId = mycol.insert_many(data)
+    return insertedId
+
